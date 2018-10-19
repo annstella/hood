2413b05ff5584c3a2aeb300341359d631f8ebba6
Annstella <annstellawangui@gmail.com>
2018-10-19 16:47:20 +0300

    added appconfigurations

--- hood/views.py
+++ hood/views.py
@@ -3,4 +3,5 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('neighbourhood')
+    hood = Business.objects.all() 
+    return render(request,'index.html', locals())
