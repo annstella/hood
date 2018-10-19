995cf426d45162e1cf486cf6747c66edd090bb3f
Annstella <annstellawangui@gmail.com>
2018-10-19 16:48:22 +0300

    added appconfigurations

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news.apps.NewsConfig',
+    'news.apps.HoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
