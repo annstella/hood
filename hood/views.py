61e254f157059f8702a359699778cca5cc5b6799
Annstella <annstellawangui@gmail.com>
2018-10-19 17:02:34 +0300

    created base.html

--- hood/views.py
+++ hood/views.py
@@ -3,5 +3,5 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    hood = Business.objects.all() 
+    # hood = Business.objects.all() 
     return render(request,'index.html', locals())
