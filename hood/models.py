7dd772ca29ce9b7976c06256ad41928400dfc75b
Annstella <annstellawangui@gmail.com>
2018-10-21 19:24:17 +0300

    added models for profile and neighbourhood

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+import datetime as dt
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length =30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    email = models.EmailField()
+
+
+    @receiver(post_save,sender=User)
+    def create_profile(sender, instance,created,**kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save,sender=User)
+    def save_profile(sender, instance,**kwargs):
+        instance.profile.save()
+    def __str__(self):
+        return self.user.username
+    def save_profile(self):
+        self.save()  
+
+    def delete_profile(self):
+        self.delete()  
+
+    @classmethod
+    def update_profile(cls,update):
+        pass
+     
+    @classmethod
+    def search_by_profile(cls,name):
+        profile = Profile.objects.filter(user__username__icontains=name)
+        return profile 
+    @classmethod 
+    def get_by_id(cls,id):
+        profile = Profile.objects.get(user = id)
+        return profile
+
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length =30)
+    location= models.CharField(max_length =30)
+    Occupants_Count= models.CharField(max_length = 30, blank=True)
+    user = models.ForeignKey(User)
+   
+    def __str__(self):
+        return self.user.username
+
+    def save_neigborhood(self):
+        self.save()  
+
+    def delete_neigborhood(self):
+        self.delete()  
+
+    @classmethod
+    def update_neigborhood(cls,update):
+        pass
+     
+    @classmethod
+    def search_by_neigborhood(cls,name):
+        neigborhood = Neigborhood.objects.filter(user__username__icontains=name)
+        return neigborhood
+    @classmethod 
+    def get_by_id(cls,id):
+        neigborhood = Neigborhood.objects.get(user = id)
+        return neigborhood 
