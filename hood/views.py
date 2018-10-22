90c89910670fdabb17d5716f7141980e39610e8c
Annstella <annstellawangui@gmail.com>
2018-10-22 03:33:02 +0300

    added search file

--- hood/views.py
+++ hood/views.py
@@ -21,3 +21,26 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-hood/search.html',{"message":message})
+
+def business(request, user_id):
+    """
+    Function that enables one to see their profile
+    """
+    title = "Business"
+    businesses = User.objects.get(id=user_id)
+    user = User.objects.get(id=user_id)
+    return render(request, 'business.html',{'title':title,"businesses":businesses})
+
+# def new_business(request):
+#     current_user = request.user
+#     businesses=Business.objects.get(user=request.user)
+#     hood= Business.objects.get(user=request.user)
+#     if request.method == 'POST':
+#         form = BusinessForm(request.POST, request.FILES,instance=request.user.profile)
+#         if form.is_valid():
+#             form.save()
+#         return redirect('/')
+
+#     else:
+#         form = BusinessForm()
+#     return render(request, "new_business.html", {"form":form}) 
